b59253783e030e98cc6f40da116ce90af92a0535
jalpa9200 <jalpa9200@gmail.com>
2023-08-27 16:53:08 +0530

    code

---
+++ client/src/main.jsx
@@ -0,0 +1,22 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+import React from 'react'
+import {render} from "react-dom";
+import App from './App';
+
+
+ const container = document.getElementById("root");
+render(
+ <React.StrictMode>
+ <App />
+  </React.StrictMode>,
+   container
+ );
